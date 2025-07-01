a66dd973d286f643e1ebe7eeb6782792e78d6a64
arjunchander1999 <134402975+arjunchander1999@users.noreply.github.com>
2025-07-01 14:08:42 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? "0.0.0.0");
 
 }
 bootstrap();
